3265de8e3036828819b778a20a2cbcf05aa9667d
Mark Grebe <mark.grebe@ku.edu>
2017-08-26 19:48:11 -0500

    Example clean compile

---
+++ blink/Blink.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE CPP #-}
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  System.Hardware.Haskino.SamplePrograms.Rewrite.Blink
+--                Based on System.Hardware.Arduino
+-- Copyright   :  (c) University of Kansas
+-- License     :  BSD3
+-- Stability   :  experimental
+--
+-- The /hello world/ of the arduino world, blinking the led.  This version is
+-- written with the expression based version of the commands and procedures
+-- introduced in version 0.3 of Haskino
+-------------------------------------------------------------------------------
+
+module Main where
+
+import Data.Word
+import System.Hardware.Haskino
+
+led :: Word8
+led = 13
+
+delay :: Word32
+delay = 1000
+
+blink :: Arduino ()
+blink = do
+    setPinMode led OUTPUT
+    blinkLoop
+  where
+    blinkLoop :: Arduino ()
+    blinkLoop = do
+        digitalWrite led True
+        delayMillis delay
+        digitalWrite led False
+        delayMillis delay
+        blinkLoop
+
+blinkExample :: IO ()
+blinkExample = withArduino False "/dev/cu.usbmodem1421" blink
+
+main :: IO ()
+#ifdef DEEP
+main = compileProgram blink "blink.ino"
+#else
+main = blinkExample
+#endif
